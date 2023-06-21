df2ddb059bb57152b278650683508c519ca88c83
SKGakiria <swgakiria@gmail.com>
2023-06-21 07:30:29 -0700

    Updated files README.md, models/amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""State Module for the HBNB project"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage_type
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represents the Amenity class"""
+    __tablename__ = "amenities"
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity)
+    else:
+        name = ""
