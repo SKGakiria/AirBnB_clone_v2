8d81cac90dffc7b1e8c1d20c83c4795ea6fd6bcb
SKGakiria <swgakiria@gmail.com>
2023-06-23 12:08:36 -0700

    Updated files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,9 +11,6 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.place import place_amenity
-
 classes = {"User": User, "State": State, "City": City,
            "Amenity": Amenity, "Place": Place, "Review": Review}
 
