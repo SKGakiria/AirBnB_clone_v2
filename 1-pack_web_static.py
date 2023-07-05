fef5b625ac83eac2e0692ea5b1d25221f373dc07
SKGakiria <swgakiria@gmail.com>
2023-07-05 11:55:52 -0700

    Updated files Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 """Generates a .tgz archive from the contents of the web_static folder,
 using the function do_pack."""
 from fabric.api import local
-from datetime import datetime
+from time import strftime
 
 
 def do_pack():
