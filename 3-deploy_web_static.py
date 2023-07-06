5c2141c10aedaed1afb921093d83e451e7df48fb
SKGakiria <swgakiria@gmail.com>
2023-07-06 13:02:12 -0700

    Updated Task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -10,12 +10,12 @@ env.user = 'ubuntu'
 
 def do_pack():
     """Function that generates tgz archive from web_static folder"""
+    f_name = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
+              format(f_name))
+        return ("versions/web_static_{}.tgz".format(f_name))
     except Exception:
         return None
 
